3d881d2fe7c1d2f1f33f7878e68b8a18298f74cd
[LeonSheff] <shevchun.leonid@mail.ru>
2023-03-30 17:15:29 +0500

    Третье дз

---
+++ DZ3/drugs/impl/components/Azitron.java
@@ -0,0 +1,13 @@
+package DZ3.drugs.impl.components;
+
+import DZ3.drugs.Component;
+
+public class Azitron extends Component {
+    public Azitron(String name, String weight, int power) {
+        super(name, weight, power);
+    }
+
+    public Azitron(String weight, int power) {
+        this("Азитромицин", weight, power);
+    }
+}
---
+++ DZ3/drugs/impl/components/Penicillin.java
@@ -0,0 +1,13 @@
+package DZ3.drugs.impl.components;
+
+import DZ3.drugs.Component;
+
+public class Penicillin extends Component {
+    public Penicillin(String name, String weight, int power) {
+        super(name, weight, power);
+    }
+
+    public Penicillin(String weight, int power) {
+        this("Пенициллин", weight, power);
+    }
+}

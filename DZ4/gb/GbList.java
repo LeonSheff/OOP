267ab9acfcc33842022d97d35cb8efc66d220b8b
[LeonSheff] <shevchun.leonid@mail.ru>
2023-04-01 20:33:43 +0500

    четвертое дз

---
+++ DZ4/gb/GbList.java
@@ -0,0 +1,10 @@
+package DZ4.gb;
+
+public interface GBList<T> extends Iterable<T> {
+    GBList<T> add(T t);
+    GBList<T> add(int i, T t);
+    void remove(int i);
+    T get(int i);
+    int size();
+    void update(int i, T t);
+}
